3f1007f2ddfce123002d8d29d56e86cf3d9cb9cf
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-02 08:58:48 +0530

    stringmethods

--- variable.js
+++ variable.js
@@ -1,11 +1,39 @@
-
 // declare a variable
-var firstName = "Anvesh";
+// let firstName = "Anvesh";
+// firstName = "Mishra";
 
 // use a variable
-console.log(firstName);
+// console.log(firstName);
 
-// change value of variable
-firstName = "Apurva";
+// change value
+//firstName = "Apurva";
 
-console.log(firstName);
+//console.log(firstName);
+
+// declare constants
+// const pi = 3.14
+
+// console.log(pi * 2)
+
+// String indexing
+// let firstName = "Anvesh";
+
+// console.log(firstName[3]);
+
+// console.log(firstName.length);
+
+// // trim()
+// let firstName = "  Anvesh      ";
+
+// console.log(firstName.length);
+// let newString = firstName.trim();
+// console.log(newString);
+// console.log(newString.length);
+
+// to uppercase
+let firstName = "anvesh";
+// firstName = firstName.toUpperCase(); // strings are immutable
+// console.log(firstName); //hence store in new variable
+
+// firstName = firstName.toLowerCase();
+// console.log(firstName);
