cd1d9e7cf442babb8857d8414ca212826423f575
anvy87 <anvesh87@gmail.com>
2022-02-02 08:53:13 +0530

    indexing

--- variable.js
+++ variable.js
@@ -11,9 +11,9 @@
 //console.log(firstName);
 
 // declare constants
-const pi = 3.14
+// const pi = 3.14
 
-console.log(pi * 2)
+// console.log(pi * 2)
 
 // String indexing
 // let firstName = "Anvesh";
@@ -22,8 +22,18 @@ console.log(pi * 2)
 
 // console.log(firstName.length);
 
-// trim()
-let firstName = "  Anvesh      ";
+// // trim()
+// let firstName = "  Anvesh      ";
 
-console.log(firstName.length);
-let newString = firstName.trim();
+// console.log(firstName.length);
+// let newString = firstName.trim();
+// console.log(newString);
+// console.log(newString.length);
+
+// to uppercase
+let firstName = "anvesh";
+// firstName = firstName.toUpperCase(); // strings are immutable
+// console.log(firstName); //hence store in new variable
+
+// firstName = firstName.toLowerCase();
+// console.log(firstName);
