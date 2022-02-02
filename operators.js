3121b6cdc73e93f9cedae76fce735d2d7b457d50
anvy87 <anvesh87@gmail.com>
2022-02-02 09:50:38 +0530

    commit

---
+++ operators.js
@@ -0,0 +1,21 @@
+// type of operators
+
+// data types (primitve data type)
+// string "anvesh"
+// numbers 2, 5, 6.7
+// booleans
+// undefined
+// null
+// BigInt
+// Symbol
+
+let age = 34;
+let firstName = "anvesh";
+
+console.log(typeof age);
+console.log(typeof firstName);
+
+// convert number to string
+console.log(typeof(age + "")); // 22 --> "22"
+
+// convert string to number
