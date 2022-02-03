6a6a7c5f33799f79e049110dbbef5ff3b1a88d70
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-03 07:35:01 +0530

    string concatenation

---
+++ concatenation.js
@@ -0,0 +1,15 @@
+// string concatenation
+
+let string1 = "anvesh";
+let string2 = "mishra";
+
+let fullName = string1 + " " + string2;
+console.log(fullName);
+
+let str1 = "17";
+let str2 = "10";
+let newStr = str1 + str2;
+console.log(newStr);
+
+let newStr1 = +str1 + +str2;
+console.log(newStr1);
