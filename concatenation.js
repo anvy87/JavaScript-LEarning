bc4a64b47b9c22eb7559eb3a4a4ef54dbd109bcd
anvy87 <anvesh87@gmail.com>
2022-02-03 07:40:09 +0530

    concatenat

--- concatenation.js
+++ concatenation.js
@@ -5,3 +5,11 @@ let string2 = "mishra";
 
 let fullName = string1 + " " + string2;
 console.log(fullName);
+
+let str1 = "17";
+let str2 = "10";
+let newStr = str1 + str2;
+console.log(newStr);
+
+let newStr1 = +str1 + +str2;
+console.log(newStr1);
