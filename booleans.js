3011163a3b25c6902a7cad324e5ac246bae1e49f
anvy87 <anvesh87@gmail.com>
2022-02-04 08:40:07 +0530

    boolean

---
+++ booleans.js
@@ -0,0 +1,12 @@
+// booleans & comparison operator
+
+let num1 = 8;
+let num2 = 7;
+
+// console.log(num1 < num2);
+
+// == vs ===
+console.log(num1 == num2); // == checks value, === checks datatype
+
+// != vs !==
+console.log(num1 != num2);
