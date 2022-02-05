0266e071341a85acd19b7c08a9dcb8547b028c30
anvy87 <anvesh87@gmail.com>
2022-02-05 08:09:18 +0530

    add

--- booleans.js
+++ booleans.js
@@ -10,3 +10,21 @@ console.log(num1 == num2); // == checks value, === checks datatype
 
 // != vs !==
 console.log(num1 != num2);
+console.log(num1 !== num2);
+
+
+// if else condition
+let age = 18;
+if (age >= 18) {
+    console.log("User can play");
+} else {
+    console.log("User can not play");
+}
+
+let num = 13;
+
+if (num % 2 === 0) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
