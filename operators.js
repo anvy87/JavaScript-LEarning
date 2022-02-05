993372e587c34478b7dfbb3380eeb2f407f5878e
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-05 08:11:03 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,27 @@
+// type of operators
+
+// data types (primitve data type)
+// string "anvesh"
+// numbers 2, 5, 6.7
+// booleans
+// undefined
+// null
+// BigInt
+// Symbol
+
+let age = 34;
+let firstName = "anvesh";
+
+console.log(typeof age);
+console.log(typeof firstName);
+
+// convert number to string
+console.log(typeof(age + "")); // 22 --> "22"
+
+// convert string to number
+let myStr = +"34";
+console.log(typeof myStr);
+
+
+age = String(age);
+console.log(typeof age);
