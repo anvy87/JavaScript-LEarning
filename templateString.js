a0c323c49a8fa4f9bc6241e3975ed1461b6e3561
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-06 22:47:33 +0530

    Add files via upload

---
+++ templateString.js
@@ -0,0 +1,10 @@
+// template string
+
+let age = 34;
+let firstName = "anvesh";
+
+// "my name is anvesh and my age is 35"
+// let aboutMe = "my name is " + firstName + " and my age is " + age
+let aboutMe = `my name is ${firstName} and my age is ${age}`
+
+console.log(aboutMe);
