d0d32bd5f9f6f3fe4025e95ceb34d42e40681172
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-07 21:42:04 +0530

    Add files via upload

---
+++ undefinedNull.js
@@ -0,0 +1,19 @@
+// undefined
+// null
+
+// let firstName;
+// console.log(typeof firstName);
+// firstName = "Anvesh";
+// console.log(typeof firstName);
+
+let myVariable = null;
+console.log(myVariable);
+console.log(typeof myVariable); // object, its a bug
+
+//BigInt
+let myNumber = 123;
+// console.log(myNumber);
+console.log(Number.MAX_SAFE_INTEGER);
+
+let myNum = BigInt(111111111111111111111111111111111111111111111);
+console.log(myNum);
