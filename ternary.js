328e39b2b30fb5c72faf8f4231a7a4ce08a38069
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-12 23:54:13 +0530

    Add files via upload

---
+++ ternary.js
@@ -0,0 +1,19 @@
+// let age = 1;
+// let drink;
+
+// if (age >= 5) {
+//     drink = "coffee";
+// } else {
+//     drink = "milk";
+// }
+
+// console.log(drink);
+
+
+
+// insteda of all the above lines
+// use ternary operators
+
+let age = 1;
+let drink = age >= 5 ? "coffee" : "milk";
+console.log(drink);
