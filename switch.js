9220fa29230535cedcb1282cf2ff0f50ac85ff77
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-14 21:08:09 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,40 @@
+// switch statement
+
+let day = 1;
+
+// if (day === 0) { // with if else
+//     console.log("Sunday");
+// } else if (day === 1) {
+//     console.log("Monday");
+// } else if (day === 2) {
+//     console.log("Tuesday");
+// } else if (day === 3) {
+//     console.log("Wednesday");
+// } else if (day === 4) {
+//     console.log("Thursday");
+// } else if (day === 5) {
+//     console.log("Friday");
+// } else if (day === 6) {
+//     console.log("Saturday");
+// } else {
+//     console.log("Invalid Day");
+// }
+
+switch (day) {
+    case 0:
+        console.log("Sunday");
+    case 1:
+        console.log("Monday");
+    case 2:
+        console.log("Tuesday");
+    case 3:
+        console.log("Wednesday");
+    case 4:
+        console.log("Thursday");
+    case 5:
+        console.log("Friday");
+    case 6:
+        console.log("Saturday");
+    default:
+        console.log("Invalid day")
+}
