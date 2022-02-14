1e1622122bca40f30cac53126ca12424521bf332
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-15 02:02:16 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,31 @@
+// 0 to 9
+// let i = 0;
+
+
+// console.log(i);
+
+// i++;
+// console.log(i);
+
+// i++;
+// console.log(i); // goes on till 9
+
+// dont repeat yourself DRY
+
+// while (i <= 3) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// example
+// first 10 natural number sum
+
+let num = 10;
+let sum = 0;
+let i = 0;
+while (i <= 10) {
+    sum = sum + i;
+    i++;
+}
+console.log(sum);
