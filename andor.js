f0fdbf487e740a5ca73624bcbcbcf8b3242201c3
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-21 21:36:18 +0530

    Add files via upload

---
+++ andor.js
@@ -0,0 +1,17 @@
+// 'and' 'or' operator
+
+let firstName = "Anvesh";
+let age = 34;
+
+// if (firstName[0] === "A") {
+//     console.log("your name starts with A")
+// }
+
+// if (age > 18) {
+//     console.log("you are above 18");
+// }
+
+
+if (firstName[0] === "A" && age > 18) {
+    console.log("Name starts with A and age is above 18");
+}
