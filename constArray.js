f1e6aba35ad4a5c34d4dc511c86f42d0ed705b30
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-02-23 09:25:08 +0530

    Add files via upload

---
+++ constArray.js
@@ -0,0 +1,28 @@
+// const fruits = ["apple", "banana"]
+
+// fruits.push("mango");
+// console.log(fruits);
+
+// while loop in array
+
+const fruits = ["apple", "banana", "mango"];
+
+// let i = 0;
+// while (i < fruits.length) {
+//     console.log(fruits[i]);
+//     i++;
+// }
+
+
+
+// for of loop in array
+// for (let fruit of fruits) {
+//     console.log(fruit);
+// }
+
+// for in loop in array
+const fruits2 = [];
+
+for (let index in fruits) {
+    console.log(index);
+}
--- primitive&reference.js
+++ primitive&reference.js
@@ -10,11 +10,24 @@
 // //reference types
 // //array
 
+// let array1 = ["item1", "item2"];
+// let array2 = array1;
+// console.log("array1", array1);
+// console.log("array2", array2);
+// array1.push("item3");
+// console.log("after pushing element to array1");
+// console.log("array1", array1);
+// console.log("array2", array2);
+
+// clone array
 let array1 = ["item1", "item2"];
-let array2 = array1;
-console.log("array1", array1);
-console.log("array2", array2);
+// let array2 = ["item1", "item2"];  // not good if many elements in arrays
+// let array2 = array1.slice(0);       // instead do this
+// let array2 = [].concat(array1);        // can also do this
+
+//spread operator.. new way
+let array2 = [...array1];
 array1.push("item3");
-console.log("after pushing element to array1");
-console.log("array1", array1);
-console.log("array2", array2);
+console.log(array1 === array2);
+console.log(array1);
+console.log(array2);
