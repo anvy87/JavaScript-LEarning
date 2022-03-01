3d60c886aefbb2a9f64da73a1650725aaf40bb8d
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-03-01 19:42:26 +0530

    Add files via upload

---
+++ arrayDestructuring.js
@@ -0,0 +1,9 @@
+const myArray = ["value1", "value2"];
+// let myVar1 = myArray[0];
+// let myVar2 = myArray[1];
+// console.log("value of myVar1:", myVar1);
+// console.log("value of myVar2:", myVar2);
+
+let [myvar1, myvar2] = myArray; // shortcut for above steps
+console.log("value of myVar1:", myvar1);
+console.log("value of myVar2:", myvar2);
