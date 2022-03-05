407175e975affa7dca2e6e0f59de5a3699062679
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-03-05 22:36:52 +0530

    Update andor.js

--- andor.js
+++ andor.js
@@ -7,9 +7,9 @@ let age = 34;
 //     console.log("your name starts with A")
 // }
 
-// if (age > 18) {
-//     console.log("you are above 18");
-// }
+if (age > 18) {
+    console.log("you are above 18");
+}
 
 
 if (firstName[0] === "A" && age > 18) {
