8097f4aacf256e7e69ab4a036c6e61957587f586
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-03-12 21:59:14 +0530

    Add files via upload

---
+++ hoisting.js
@@ -0,0 +1,10 @@
+hello();
+
+function hello() { // function declaration
+    console.log("hello world");
+}
+
+hello1();
+const hello1 = function() { // function expression
+    console.log("hello world");
+}
