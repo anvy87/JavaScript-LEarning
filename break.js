b2ed1ec97b2b0b38beb90bd0a2bd3f2dd35b60cf
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-04-12 23:37:33 +0530

    Update break.js

--- break.js
+++ break.js
@@ -4,12 +4,10 @@ for (let i = 1; i <= 10; i++) {
      }
      console.log(i); // number 4 is not printed
 } // bcz of BREAK
-
 for (let i = 1; i <= 10; i++) {
     if (i === 4) {
         continue;
     }
     console.log(i);
 }
-
 console.log("Hello!");
