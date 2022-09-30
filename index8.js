f13b30e1aac009aad4c98310074694fcb3f94c33
Anvesh Mishra <56150929+anvy87@users.noreply.github.com>
2022-09-30 08:55:30 +0530

    Add files via upload

---
+++ index8.js
@@ -0,0 +1,20 @@
+// // travessing a tree
+
+// const rootNode = document.getRootNode();
+// const htmlElementNode = rootNode.childNodes[0];
+// console.log(htmlElementNode);
+// console.log(htmlElementNode.childNodes);
+
+// const headElementNode = htmlElementNode.childNodes[0];
+// const textNode1 = htmlElementNode.childNodes[1];
+// const bodyElementNode = htmlElementNode.childNodes[2];
+// console.log(headElementNode.nextElementSibling);
+
+// sibling relation
+// const h1 = document.querySelector("h1");
+// const body = h1.parentNode.parentNode;
+// body.style.color = "#efefef";
+// body.style.backgroundColor = "#333"
+
+const body = document.body
+console.log(body)
